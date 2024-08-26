0c0d7f99c1331c59a8a2a0a14ecce591bf4c5ae9
KirichenkoYV <hause99@gmail.com>
2024-08-26 12:19:51 +0300

    LS-002.Правки header

--- src/shared/components/Layout/components/Header/Header.tsx
+++ src/shared/components/Layout/components/Header/Header.tsx
@@ -8,12 +8,14 @@ import s from './Header.module.scss';
 const Header = memo(() => {
   const { pathname } = useLocation();
 
+  console.log(pathname);
+
   const title = useMemo(() => getHeaderTitle(pathname), [pathname]);
 
   return (
     <div className={s.header}>
       <span className={s.headerTitle}>{title}</span>
-      {!pathname && (
+      {pathname === '/' && (
         <span className={s.headerDate}>
           {getCurrentYearAndMonth().month}, {getCurrentYearAndMonth().year}
         </span>
